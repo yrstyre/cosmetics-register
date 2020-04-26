66b036ef8b5c7c5910d1f4291a04c80042eac5e9
Emy Spjuth <emy.spjuth@prisjakt.nu>
2020-04-26 21:34:32 +0200

    Inits project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Cosmetics register</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
